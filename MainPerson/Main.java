fc9e5d9036ac8e9ded021b01e2a20e2bf629deb2
yoshi3939 <irieyoshimi43@gmail.com>
2021-11-07 19:00:50 +0900

    例外、ラムダについて

---
+++ MainPerson/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+  public static void main(String[] args){
+    MainPerson p = new MainPerson();
+    p.setAge(-10);
+  }
+}
---
+++ MainPerson/MainPerson.java
@@ -0,0 +1,9 @@
+public class MainPerson {
+  int age;
+  public void setAge(int age){
+    if(age < 0) {
+      throw new IllegalArgumentException("年齢は0以上の数を指定すべきです。指定値=" + age);
+    }
+    this.age = age;
+  }
+}
